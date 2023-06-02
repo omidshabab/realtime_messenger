691d7566509eaf69b383f0e73c92da8546b4c2f1
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-06-02 00:49:53 -0700

    first commit

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { signOut } from "next-auth/react";
+
+const Users = () => {
+    return ( 
+        <button onClick={() => signOut()}>
+            Logout
+        </button>
+    );
+}
+ 
+export default Users;

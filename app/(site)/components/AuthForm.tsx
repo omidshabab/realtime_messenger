1d0e9bb10e544b8c2a0a63e129d3730f1574c2a3
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-06-01 23:12:16 -0700

    first commit

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import axios from "axios";
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
@@ -7,6 +8,8 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 
 import { BsGithub, BsGoogle } from "react-icons/bs"
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react"
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -41,10 +44,27 @@ const AuthForm = () => {
 
         if(variant === "REGISTER"){
             // Axios Register
+            axios.post('/api/register', data)
+            .catch(() => toast.error('Something went wrong!'))
+            .finally(() => setIsLoading(false))
         }
 
         if(variant === "LOGIN"){
             // NextAuth SignIn
+            signIn('credentials', {
+                ...data,
+                redirect: false
+            })
+            .then((callback) => {
+                if(callback?.error){
+                    toast.error('Invalid credentials')
+                }
+
+                if(callback?.ok && !callback.error){
+                    toast.success('Logged in!')
+                }
+            })
+            .finally(() => setIsLoading(false))
         }
     } 
 

8dcc20a69381e11b59bcb45a20c4437ef66d77e9
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-06-02 12:03:01 -0700

    first commit

---
+++ app/users/components/UserBox.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const UserBox = () => {
+    return ( 
+        <div>
+            User Box
+        </div>
+     );
+}
+ 
+export default UserBox;
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Sidebar from "../components/sidebar/Sidebar"
+import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
     children
 }: {
     children: React.ReactNode
 }) {
+    const users = await getUsers()
+
     return (
         // @ts-expect-error Server Component
         <Sidebar>
             <div className="h-full">
+                <UserList items={users} />
                 {children}
             </div>
         </Sidebar>
